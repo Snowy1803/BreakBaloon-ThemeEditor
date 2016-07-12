28173cd05a1e1ce42c592f0e26e50b3c23047742
Emil <3mille.prenom.nom@gmail.com>
2016-07-12 21:18:14 +0200

    Undo/Redo now working with fill tool

--- src/st/infos/elementalcube/breakbaloon/theme/editor/drawing/DrawingToolFill.java
+++ src/st/infos/elementalcube/breakbaloon/theme/editor/drawing/DrawingToolFill.java
@@ -1,5 +1,7 @@
 package st.infos.elementalcube.breakbaloon.theme.editor.drawing;
 
+import st.infos.elementalcube.breakbaloon.theme.editor.drawing.UndoableImageEdit.UndoableImageEditBuilder;
+
 import java.awt.Color;
 import java.awt.Point;
 import java.awt.image.BufferedImage;
@@ -13,25 +15,33 @@ public class DrawingToolFill extends DrawingTool {
 	
 	@Override
 	public boolean draw(DrawEditor editor, Point from, Point to, Color color) {
-		return propagate(editor.getImage(), to, new Color(editor.getImage().getRGB(to.x, to.y), true), color);
+		UndoableImageEditBuilder builder = new UndoableImageEditBuilder();
+		propagate(editor.getImage(), to, new Color(editor.getImage().getRGB(to.x, to.y), true), color, builder);
+		UndoableImageEdit edit = builder.toUndoableImageEdit(editor);
+		if (edit != null) {
+			editor.undoManager.addEdit(edit);
+			return true;
+		}
+		return false;
 	}
 
-	private boolean propagate(BufferedImage image, Point point, Color seekingColor, Color color) {
+	private boolean propagate(BufferedImage image, Point point, Color seekingColor, Color color, UndoableImageEditBuilder builder) {
 		if (seekingColor == color) {
 			return false;
 		}
+		builder.add(point, new Color(image.getRGB(point.x, point.y), true), color);
 		image.setRGB(point.x, point.y, color.getRGB());
 		if (point.x + 1 < image.getHeight() && image.getRGB(point.x + 1, point.y) == seekingColor.getRGB()) {
-			propagate(image, new Point(point.x + 1, point.y), seekingColor, color);
+			propagate(image, new Point(point.x + 1, point.y), seekingColor, color, builder);
 		}
 		if (point.x > 0 && image.getRGB(point.x - 1, point.y) == seekingColor.getRGB()) {
-			propagate(image, new Point(point.x - 1, point.y), seekingColor, color);
+			propagate(image, new Point(point.x - 1, point.y), seekingColor, color, builder);
 		}
 		if (point.y + 1 < image.getHeight() && image.getRGB(point.x, point.y + 1) == seekingColor.getRGB()) {
-			propagate(image, new Point(point.x, point.y + 1), seekingColor, color);
+			propagate(image, new Point(point.x, point.y + 1), seekingColor, color, builder);
 		}
 		if (point.y > 0 && image.getRGB(point.x, point.y - 1) == seekingColor.getRGB()) {
-			propagate(image, new Point(point.x, point.y - 1), seekingColor, color);
+			propagate(image, new Point(point.x, point.y - 1), seekingColor, color, builder);
 		}
 		return true;
 	}
